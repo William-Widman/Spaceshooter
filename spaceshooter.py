077832368e9ce68b541b961a9d58f4db946a22b3
William Widman <william.widman@learnet.se>
2025-02-26 16:04:44 +0100

    första kommit

---
+++ spaceshooter.py
@@ -0,0 +1,100 @@
+import pygame
+import os
+pygame.font.init()
+
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space shooter Tutorial")
+
+# Load images
+RED_SPACE_SHIP = pygame.image.load(os.path.join("pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("pixel_ship_blue_small.png"))
+
+
+# Player player
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("pixel_ship_yellow.png"))
+
+# Lasers
+RED_LASER = pygame.image.load(os.path.join("pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("pixel_laser_blue.png"))
+YELLOW_LASERS = pygame.image.load(os.path.join("pixel_laser_yellow.png"))
+
+
+# Background
+BG = pygame.transform.scale(pygame.image.load(os.path.join("background-black.png")), (WIDTH, HEIGHT))
+
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, val):
+        self.y += val
+
+    def off_screen(self, height):
+        return not(self.y <= height and self.y >= 0)
+    
+    def collision(self, obj):
+        return collide(self, obj)
+    
+class Ship:
+    COOLDOWN = 30
+
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers =  []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+        for laser in self.lasers:
+            laser.draw(window)
+            
+    def move_lasers(self, vel, obj):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            elif laser.collision(obj):
+                obj.health -= 10
+                self.lasers.remove(laser)
+
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+                self.cool_down_counter = 0
+            elif self.cool_down_counter > 0:
+                self.cool_down_counter += 1
+
+        def shoot(self):
+            if self.cool_down_counter == 0:
+                laser = Laser(self.x, self.y, self.laser_img)
+                self.lasers.append(laser)
+                self.cool_down_counter = 1
+
+        def get_width(self):
+            return self.ship_img.get_width()
+        
+        def get_height(self):
+            return self.ship_img.get_height()
+        
+
+    class
+            
+
+
+pygame.display.init()
+
+
+pygame.quit()
